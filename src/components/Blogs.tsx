d472cc219788a5b643ad4e840ac729b0ae3eddde
yssh-raj10 <yashraj.se10@gmail.com>
2023-11-18 14:49:28 +0530

    .

--- src/components/Blogs.tsx
+++ src/components/Blogs.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Blog from "./Blog";
 import { getPosts } from "@/utils/sanity-utils";
--- src/components/Tool.tsx
+++ src/components/Tool.tsx
@@ -13,10 +13,10 @@ export default function Tool({ props }: Props) {
   return (
     <div>
       <div className="hover:scale-105 duration-300">
-        <img
-          // initial={{ y: -50, opacity: 0 }}
-          // whileInView={{ y: 0, opacity: 1 }}
-          // transition={{ duration: 1 }}
+        <motion.img
+          initial={{ y: -50, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1 }}
           src={props.image}
           alt=""
           className=" w-24 h-24  "

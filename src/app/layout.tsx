9c23e93982cab9af1511eeef635ec6c37d08b36e
yssh-raj10 <yashraj.se10@gmail.com>
2023-11-18 14:52:54 +0530

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 "use client";
 import "./globals.css";
-import type { Metadata } from "next";
+
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Protfolio",
-  description: "Protfolio 2.0",
-};
 
 export default function RootLayout({
   children,
